6b9d52b28d0db989f08acd4a77f0b738c2b27c3b
Shautek <zs6sobieski.edu.pl>
2024-03-18 12:29:49 +0100

    Update commit

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-let iloscZapalek = prompt("Podaj ilość zapałek");
 const zapalki = document.querySelectorAll(".zapalka");
 const div = document.querySelector("#napisy");
 let gracz = true;
+const play = document.querySelector("#play");
+const rules = document.querySelector("#rules");
+let iloscZapalek = 0;
+const rules_window = document.querySelector("#rules_window");
 
-div.innerHTML = "<h2>Ilośc zapałek do wzięcia: </h2><h1>" + iloscZapalek + "</h1>";
-div.innerHTML += "<h2>Ruch gracza pierwszego</h2>";
+rules.addEventListener("click", function()
+{
+    if(rules_window.style.visibility == "hidden")
+    {
+        rules_window.style.visibility = "visible";
+    }
+    else
+    {
+        rules_window.style.visibility = "hidden";
+    }
+})
 
+play.addEventListener("click", function()
+{
+    iloscZapalek = prompt("Podaj ilość zapałek");
+    div.innerHTML = "<h2>Ilośc zapałek do wzięcia: </h2><h1>" + iloscZapalek + "</h1>";
+    div.innerHTML += "<h2>Ruch gracza pierwszego</h2>";
+})
 zapalki.forEach(element => {
     element.addEventListener("click", function()
     {
-        if(iloscZapalek >= element.id)
+        console.log(element.id);
+        if(parseInt(iloscZapalek) >= parseInt(element.id))
         {
             iloscZapalek -= element.id;
             if(iloscZapalek != 0)
